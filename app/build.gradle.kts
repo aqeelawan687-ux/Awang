4a9a6030bb37346876843ffc622beabc8c46847f
aqeelawan687-ux <aqeelawan687-ux@users.noreply.github.com>
2026-08-25 17:11:50 +0500

    Bump app version to 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.aistudio.aqeelrider.kxmpzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 132
-    versionName = "1.3.2"
+    versionCode = 133
+    versionName = "1.3.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
